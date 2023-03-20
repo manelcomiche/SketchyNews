3ed715c3a45e42690428cfb0d2dba16cd807e842
LHCLYT <46402841+LHCLYT@users.noreply.github.com>
2023-03-20 23:17:00 +0100

    express

---
+++ src/Server/server.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+
+import express from 'express'
+const app = express();
+
+import midjourney from './utils/midjourneyGenerator.js';
+
+import path from 'path';
+global.__dirname = path.resolve();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, '/src/Web/public')));
+
+app.get('/', (req, res) => { res.sendFile('index.html'); })
+
+app.get('/api/generate', async (req, res) => {
+    const prompt = req.query.prompt;
+
+    const generated = await midjourney(`mdjrny-v4 Based on the following description: ${prompt} generate me an artistic image of what I entered putting your own vision of it.`)
+
+    res.send(generated)
+
+    console.log(generated)
+
+});
---
+++ src/Server/utils/midjourneyGenerator.js
@@ -0,0 +1,41 @@
+import { fetch, CookieJar } from 'node-fetch-cookies';
+
+function sleep(milliseconds) { return new Promise(resolve => setTimeout(resolve, milliseconds)) }
+
+export default async function midjourney(prompt, inputs = {}) {
+    const sessionCookieJar = new CookieJar()
+
+    await fetch(sessionCookieJar, process.env.COOKIE_FETCH_URL)
+
+    const response1 = await fetch(sessionCookieJar, process.env.PREDICTIONS_URL, {
+        headers: { 'content-type': 'application/json', 'accept': 'application/json', 'x-csrftoken': sessionCookieJar.cookies.get('csrftoken') },
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({
+            inputs: {
+                guidance_scale: '7',
+                width: 512,
+                height: 512,
+                num_inference_steps: 50,
+                num_outputs: 1,
+                seed: null,
+                prompt,
+                ...inputs,
+            },
+        }),
+    })
+
+    const uuid = (await response1.json()).uuid
+
+    for (let timeoutCounter = 0; timeoutCounter < 10; timeoutCounter++) {
+        let response2 = await fetch(sessionCookieJar, `${process.env.PREDICTIONS_URL}/${uuid}`, { headers: { 'accept': '*/*' }, method: 'GET', mode: 'cors', credentials: 'include', body: null })
+
+        let output = (await response2.json())?.prediction?.output
+        if (output && output.length) { return output }
+
+        await sleep(1000)
+    }
+
+    return []
+}
