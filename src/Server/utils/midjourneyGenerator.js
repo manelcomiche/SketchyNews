cdf190091825f0a442ba05355f6f5352f3b194f4
LHCLYT <46402841+LHCLYT@users.noreply.github.com>
2023-04-10 19:57:59 +0200

    test mid 2

--- src/Server/utils/midjourneyGenerator.js
+++ src/Server/utils/midjourneyGenerator.js
@@ -1,32 +1,37 @@
-import fetch from 'chiaki'
+import axios from 'axios';
 
 export default {
     run: async function (model, inputs) {
-        let prediction = await this.create(model, inputs)
-
+        let prediction;
+        try { prediction = await this.create(model, inputs); }
+        catch (e) { throw e.response.data; }
         while (!['canceled', 'succeeded', 'failed'].includes(prediction.status)) {
-            await new Promise(_ => setTimeout(_, 250))
-            prediction = await this.get(prediction)
+            await new Promise(_ => setTimeout(_, 250));
+            prediction = await this.get(prediction);
         }
 
-        return prediction.output
+        return prediction.output;
     },
 
-    get(prediction) {
-        return fetch(`https://replicate.com/api/models${prediction.version.model.absolute_url}/versions/${prediction.version_id}/predictions/${prediction.uuid}`).then(response => JSON.parse(response.body).prediction)
+    async get(prediction) {
+        if (prediction.prediction) return prediction.prediction;
+        const controller = new AbortController();
+        const id = setTimeout(() => controller.abort(), 29000);
+        const response = await axios.get(`https://replicate.com/api/models${prediction.version.model.absolute_url}/versions/${prediction.version_id}/predictions/${prediction.uuid}`, { signal: controller.signal }).then(r => r.data);
+        clearTimeout(id);
+        return response;
     },
 
     create(model, inputs) {
-        const [path, version] = model.split(':')
+        const [path, version] = model.split(':');
 
-        return fetch({
-            hostname: 'replicate.com',
-            path: `/api/models/${path}/versions/${version}/predictions`,
+        return axios({
+            url: `https://replicate.com/api/models/${path}/versions/${version}/predictions`,
             method: 'POST',
             headers: {
-                'content-type': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ inputs }),
-        }).then(response => JSON.parse(response.body))
+            data: JSON.stringify({ inputs })
+        }).then(response => response.data);
     }
-}
+};
